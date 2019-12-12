a9e61aeba1af56f4c92b1e9b50755e04f7d69f0f
squashgray <squashgray@gmail.com>
2019-12-11 17:40:19 -0800

    reducer + store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import "bulma/css/bulma.css";
+import "./styles.scss";
+const store = createStore(reducer);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
